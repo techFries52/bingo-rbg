c42f60d2a89b7460eb9bee8fcd66a335c3d0cecd
jjlrevature <69030071+techFries52@users.noreply.github.com>
2022-07-26 12:24:23 -0500

    up

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/client'));
+app.use(express.static(__dirname + 'dist/client'));
 
 app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname,
-    '/dist','client', 'index.html'));});
+    'dist','client', 'index.html'));});
 
 app.listen(process.env.PORT || 4200);
