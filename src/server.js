dd19a8d173b9a16ea899666a5f674ffc9d99883d
jjlrevature <69030071+techFries52@users.noreply.github.com>
2022-07-26 10:17:52 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(__dirname + '/dist/client'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/rbg-bingo/index.html'));});
+'/dist/client/index.html'));});
 app.listen(process.env.PORT || 4200);
