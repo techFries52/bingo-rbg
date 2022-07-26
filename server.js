7bb1e3fc2848ec0c963e666564db98a8e2bed079
jjlrevature <69030071+techFries52@users.noreply.github.com>
2022-07-26 12:10:36 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist/client'));
 
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+
+    res.sendFile(path.join(__dirname,
     'dist','client', 'index.html'));});
 
 app.listen(process.env.PORT || 4200);
